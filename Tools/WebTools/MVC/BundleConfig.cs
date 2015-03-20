22c3162c26d804f49cb872244b5411d177329089
ahmadali shafiee <mail@ahmadalli.net>
2015-03-20 16:56:47 +0330

    BundelConfig HtmlHelper from dotnettips.info
    this code is from http://www.dotnettips.info/Post/1008/ written by Vahid
    Nasiri

---
+++ Tools/WebTools/MVC/BundleConfig.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Ahmadalli.Tools.WebTools.MVC
+{
+    /// <summary>
+    /// A custom bundle orderer (IBundleOrderer) that will ensure bundles are 
+    /// included in the order you register them.
+    /// </summary>
+    public class AsIsBundleOrderer : IBundleOrderer
+    {
+
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+
+    /// <summary>
+    /// BundleConfigTool written by Vahid Nasiri : 
+    /// http://www.dotnettips.info/Post/1008/%D9%86%D8%AD%D9%88%D9%87-%D8%A7%D8%B1%D8%AA%D9%82%D8%A7%D8%A1-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87%E2%80%8C%D9%87%D8%A7%DB%8C-%D9%85%D9%88%D8%AC%D9%88%D8%AF-mvc3-%D8%A8%D9%87-mvc4
+    /// </summary>
+    public static class BundleConfig
+    {
+        private static void addBundle(string virtualPath, bool isCss, params string[] files)
+        {
+            BundleTable.EnableOptimizations = true;
+
+            var existing = BundleTable.Bundles.GetBundleFor(virtualPath);
+            if (existing != null)
+                return;
+
+            var newBundle = isCss ? new Bundle(virtualPath, new CssMinify()) : new Bundle(virtualPath, new JsMinify());
+            newBundle.Orderer = new AsIsBundleOrderer();
+
+            foreach (var file in files)
+                newBundle.Include(file);
+
+            BundleTable.Bundles.Add(newBundle);
+        }
+
+        public static IHtmlString AddScripts(string virtualPath, params string[] files)
+        {
+            addBundle(virtualPath, false, files);
+            return Scripts.Render(virtualPath);
+        }
+
+        public static IHtmlString AddStyles(string virtualPath, params string[] files)
+        {
+            addBundle(virtualPath, true, files);
+            return Styles.Render(virtualPath);
+        }
+
+        public static IHtmlString AddScriptUrl(string virtualPath, params string[] files)
+        {
+            addBundle(virtualPath, false, files);
+            return Scripts.Url(virtualPath);
+        }
+
+        public static IHtmlString AddStyleUrl(string virtualPath, params string[] files)
+        {
+            addBundle(virtualPath, true, files);
+            return Styles.Url(virtualPath);
+        }
+    }
+}
